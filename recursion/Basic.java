e5905b9f5196928eac18bb49464f78e8d73eb499
nasirbhat762 <nasirbhat762@gmail.com>
2024-03-10 17:59:17 +0530

    basic of recursion

---
+++ recursion/Basic.java
@@ -0,0 +1,15 @@
+class Basic{
+    public static void printNum(int n){
+        if(n > 5){
+            return;
+        }
+
+        System.out.println(n);
+        printNum(n+1);
+    }
+    public static void main(String[] args) {
+        int n = 1;
+
+        printNum(n);
+    }
+}
